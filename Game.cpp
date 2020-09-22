7fd633054d08b117af811092837bd57623c12c45
tochkar <tochkaralisa@gmail.com>
2020-09-22 11:32:49 +0300

    check win added+some edits according class

--- Game.cpp
+++ Game.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include "Game.h"
 
 using namespace std;
 
+//---------------------------------------------------------------------------------//
 
+
+Game::Game(int fSize) : fieldSize(fSize){
+    maxMoves = fieldSize * fieldSize;
+    field = new int*[fieldSize];
+    for (int i = 0; i < fieldSize; i++)
+        field[i] = new int[fieldSize];
+
+    for (int i = 0; i < fieldSize; i++) // ввод
+        for (int j = 0; j < fieldSize; j++)
+        {
+            field[i][j] = 0;
+        }
+}
+
+//---------------------------------------------------------------------------------//
+
+Game::~Game(){
+    for (int i = 0; i < fieldSize; i++)
+    {
+        delete [] field[i];
+    }
+    delete [] field;
+    cout << "Game over!" << endl;
+}
+
+
+//---------------------------------------------------------------------------------//
 void Game::showField()
 {
     cout << endl;
@@ -20,22 +49,59 @@ void Game::showField()
                 if(j)cout << char(134); cout << char(173);
         }
         cout << endl;
+
     }
 }
 
+//---------------------------------------------------------------------------------//
 
-Game::Game(int fSize) : fieldSize(fSize){
-   int maxMoves = fieldSize * fieldSize;
-    field = new int*[fieldSize];
-    for (int i = 0; i < fieldSize; i++)
-        field[i] = new int[fieldSize];
-
-    for (int i = 0; i < fieldSize; i++) // ввод
-        for (int j = 0; j < fieldSize; j++)
+int Game::checkWin()
+{
+    int i, j, maxSizeO, maxSizeX;
+    for(i = 0; i < fieldSize; i++)
+    {
+        maxSizeX = 0; maxSizeO = 0;
+        for(j = 0; j < fieldSize; j++)
         {
-            field[i][j] = 0;
+            if(field[i][j] == 1)maxSizeX++; else
+            if(field[i][j] == 2)maxSizeO++;
         }
+        if(maxSizeX == fieldSize)return 1; else if(maxSizeO == fieldSize)return 2;
+    }
+    for(i = 0; i < fieldSize; i++)
+    {
+        maxSizeX = 0; maxSizeO = 0;
+        for(j = 0; j < fieldSize; j++)
+        {
+            if(field[j][i] == 1)maxSizeX++; else
+            if(field[j][i] == 2)maxSizeO++;
+        }
+        if(maxSizeX == fieldSize)return 1; else if(maxSizeO == fieldSize)return 2;
+    }
+    maxSizeX=0; maxSizeO=0;
+    for(i = 0; i < fieldSize; i++)
+    {
+        if(field[i][i] == 1)maxSizeX++; else
+        if(field[i][i] == 2)maxSizeO++;
+    }
+    if(maxSizeX == fieldSize)return 1;
+    else if(maxSizeO == fieldSize)return 2;
+
+    maxSizeX=0; maxSizeO=0;
+    for(i = 0; i < fieldSize; i++)
+    {
+        if(field[i][fieldSize - i] == 1)
+            maxSizeX++;
+        else if(field[i][fieldSize - i] == 2)
+            maxSizeO++;
+    }
+    if(maxSizeX == fieldSize)return 1;
+    else if(maxSizeO == fieldSize)return 2;
+
+    if(!maxMoves)return 3;
+    else return 0;
 }
+//---------------------------------------------------------------------------------//
 
 bool Game::checkPlace(int row, int column)
 {
